8f1ba6c57ad03b95f67f0497acb08bcae5d6d93b
mesterengineer2023 <mhamed.abaruahed2021@gmail.com>
2024-01-12 04:26:44 -0800

    0x09-static_libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: give value
+ * @c: give value
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int t = 0;
+
+	for (; s[t] >= '\0'; t++)
+
+	{
+	if (s[t] == c)
+	return (&s[t]);
+	}
+	return (0);
+}
