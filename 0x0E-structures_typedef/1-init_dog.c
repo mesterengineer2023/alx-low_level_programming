f339b976e131df81f265c967574ed192946edbd6
mesterengineer2023 <mhamed.abaruahed2021@gmail.com>
2024-01-22 16:00:57 +0100

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,7 @@
  * @d: size of Structure.
  * @name: size of name
  * @age: size of age
- * @owner: size of 
+ * @owner: size of
  * Return: always 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
